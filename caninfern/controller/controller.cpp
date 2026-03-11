ff5d6d85bbc41c71015d23fffba56849b5327510
Joaopmoliveira <joaomoliveira1996@gmail.com>
2026-03-11 20:25:00 +0000

    add controller and sensor thread. Now I will finally switch it up with the code.

--- caninfern/controller/controller.cpp
+++ caninfern/controller/controller.cpp
@@ -1,3 +1,68 @@
-int main(){
-  return 0;
+#include <array>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <iostream>
+#include <csignal>
+#include <thread>
+#include <sstream>
+#include <asio.hpp>
+#include <cmath>
+#include <type_traits>
+#include "watchdogmessage.h"
+
+
+asio::io_context io_content;
+asio::ip::tcp::socket *socket_pointer = nullptr;
+
+void signal_handler(int val)
+{
+    if (socket_pointer != nullptr)
+        socket_pointer->shutdown(asio::ip::tcp::socket::shutdown_both);
+    io_content.stop();
+}
+
+watchdog_message global_message;
+
+int main()
+{
+    std::signal(SIGINT, signal_handler);
+
+    constexpr watchdog_message_layout message_layout;
+    constexpr size_t watchdog_message_size = message_layout.image_reading_present_address + message_layout.image_reading_present_size;
+    std::array<unsigned char, watchdog_message_size> asio_memory_buffer;
+
+    unsigned int port = 50010;
+    asio::ip::tcp::endpoint endpoit(asio::ip::tcp::v4(), port);
+    asio::ip::tcp::acceptor acceptor(io_content, endpoit);
+
+    asio::error_code ec;
+    asio::ip::tcp::socket client_socket = acceptor.accept();
+    if (ec)
+    {
+        std::cout << "failed to run, terminating....\n";
+        std::cout << ec.message() << std::endl;
+        return 1;
+    };
+    socket_pointer = &client_socket;
+    
+    while (!io_content.stopped())
+    {
+        asio::read(client_socket, asio::buffer(asio_memory_buffer), asio::transfer_exactly(watchdog_message_size), ec);
+        if (ec)
+        {
+            std::printf("failed to read information\n terminating....\n");
+            io_content.stop();
+        }
+
+        watchdog_message local_message;
+        copy_from_memory_to_watchdog_message(asio_memory_buffer.data(), local_message);
+
+        asio::write(client_socket, asio::buffer(asio_memory_buffer), asio::transfer_exactly(watchdog_message_size), ec);
+        if (ec)
+        {
+            std::printf("failed to send control action\n terminating....\n");
+            io_content.stop();
+        }
+    }
 }
--- caninfern/sensors/sensors.cpp
+++ caninfern/sensors/sensors.cpp
@@ -1,3 +1,75 @@
-int main(){
-  return 0;
+#include <array>
+#include <atomic>
+#include <iostream>
+#include <csignal>
+#include <sstream>
+#include <asio.hpp>
+#include <cmath>
+#include <type_traits>
+#include "watchdogmessage.h"
+#include <random>
+
+asio::io_context io_context;
+asio::ip::tcp::socket* socket_pointer = nullptr;
+constexpr watchdog_message_layout message_layout;
+constexpr size_t watchdog_message_size = message_layout.image_reading_present_address+message_layout.image_reading_present_size;
+std::array<unsigned char,watchdog_message_size> asio_memory_buffer;
+
+void signal_handler(int val)
+{
+    if(socket_pointer!=nullptr)
+        socket_pointer->shutdown(asio::ip::tcp::socket::shutdown_both);
+    io_context.stop();
 }
+
+int main(){
+try{
+    std::signal(SIGINT,signal_handler);
+    unsigned int port = 50000;
+    asio::ip::tcp::endpoint endpoit(asio::ip::tcp::v4(), port);
+    asio::ip::tcp::acceptor acceptor(io_context,endpoit);
+
+    asio::error_code ec;
+    asio::ip::tcp::socket client_socket = acceptor.accept();
+    if (ec){
+        std::cout << "failed to run, terminating....\n";
+        std::cout << ec.message() << std::endl;
+        return 1;
+    };
+
+    socket_pointer = &client_socket;
+
+    watchdog_message message;
+   for(size_t counter = 0;!io_context.stopped(); ++counter){
+        asio::read(client_socket,asio::buffer(asio_memory_buffer),asio::transfer_exactly(watchdog_message_size),ec);
+        if(ec){
+            std::printf("failed to send information\n terminating....\n");
+            io_context.stop();
+            break;
+        } 
+        copy_from_memory_to_watchdog_message(asio_memory_buffer.data(),message);
+        std::chrono::time_point currently = std::chrono::time_point_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now()
+        );
+        std::chrono::duration millis_since_utc_epoch = currently.time_since_epoch();
+        message.sensors_receive_timestamp = millis_since_utc_epoch.count();
+        currently = std::chrono::time_point_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now()
+        );
+        millis_since_utc_epoch = currently.time_since_epoch();
+        message.sensors_send_timestamp = millis_since_utc_epoch.count();
+        copy_from_watchdog_message_to_memory(asio_memory_buffer.data(),message);
+        asio::write(client_socket,asio::buffer(asio_memory_buffer),asio::transfer_exactly(watchdog_message_size),ec);
+        if(ec){
+            std::printf("failed to send information\n terminating....\n");
+            io_context.stop();
+            break;
+        }
+        
+    }
+    std::raise(SIGINT);
+  } catch(...){
+        std::cout << "failure was detected in either communication or shared memory operation\n";
+  }
+}
+
